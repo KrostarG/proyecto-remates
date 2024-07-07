46993fd91a55c36509b4363cd4a4e15da25a00a4
Mario Gutierrez <88950695+KrostarG@users.noreply.github.com>
2024-07-06 21:52:36 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 # build.sh
 #!/bin/bash
-set -e
 
-# Leer e instalar los paquetes del archivo apt-packages
-if [ -f "apt-packages" ]; then
-  apt-get update
-  xargs apt-get install -y < apt-packages
-fi
+# Update the package list
+apt-get update
+
+# Install the necessary packages
+apt-get install -y <necessary-package>
 
 # Instalar las dependencias de Python
 pip install -r requirements.txt
+
+
